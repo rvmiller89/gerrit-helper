690da04e4a896e24fe73583f6b09d7330c9b9662
Ryan Miller <rvmiller89@gmail.com>
2020-09-08 17:22:20 -0700

    Add status bar callback; Settings button; Few icons

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,14 @@
-'use strict'
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message == 'finishedToggle') {
+    document.getElementById('progressBar').hidden = true
+  } else {
+    console.warn('unknown message received: ' + request)
+  }
+})
 
 toggleHide.onclick = function (element) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    document.getElementById('progressBar').hidden = false
     chrome.tabs.executeScript(tabs[0].id, { file: 'js/content_script.js' })
   })
 }
@@ -10,6 +17,14 @@ function updateCurrentRegexHTMLMessage() {
   chrome.storage.sync.get('fileRegex', function (data) {
     document.getElementById('currentRegex').value = data.fileRegex
   })
+}
+
+optionsButton.onclick = function (element) {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage()
+  } else {
+    window.open(chrome.runtime.getURL('options.html'))
+  }
 }
 
 updateCurrentRegexHTMLMessage()
